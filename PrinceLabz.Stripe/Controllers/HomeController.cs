185e3da70960f9956cbe405c584c010568d1b9d5
Prince-Coding-Lab <54809854+Prince-Coding-Lab@users.noreply.github.com>
2020-05-10 23:34:27 +0530

    Prince: Bind data from textbox models to controller.

--- PrinceLabz.Stripe/Controllers/HomeController.cs
+++ PrinceLabz.Stripe/Controllers/HomeController.cs
@@ -22,15 +22,19 @@ namespace PrinceLabz.Stripe.Controllers
         {
             return View();
         }
+        public IActionResult OrderDetail()
+        {
+            return View();
+        }
         [HttpPost]
         public async Task<IActionResult> Index(Customer customer)
         {
             Stripe1.CreditCardOptions card = new Stripe1.CreditCardOptions();
-            card.Name = "Prince Kumar";
-            card.Number = "4242424242424242";
-            card.ExpYear = 2025;
-            card.ExpMonth = 12;
-            card.Cvc = "123";
+            card.Name = customer.FirstName + " " + customer.LastName;
+            card.Number = customer.CardNumber;// testing card number "4242424242424242";
+            card.ExpYear = customer.ExpYear; ;
+            card.ExpMonth = customer.ExpMonth; ;
+            card.Cvc = customer.Cvc;
             //Assign Card to Token Object and create Token  
             Stripe1.TokenCreateOptions token = new Stripe1.TokenCreateOptions();
             token.Card = card;
@@ -38,16 +42,16 @@ namespace PrinceLabz.Stripe.Controllers
             Stripe1.Token tokenresult = await _stripeGateway.GetTokenAsync(card);
 
             Stripe1.CustomerCreateOptions myCustomer = new Stripe1.CustomerCreateOptions();
-            myCustomer.Email = "princesharma78@gmail.com";
-            // myCustomer.SourceToken = tokenresult.Id;
+            myCustomer.Email = customer.Email;
+
 
             Stripe1.Customer stripeCustomer = await _stripeGateway.CreateCustomerAsync(myCustomer);
-            customer.Amount = 50;
+            customer.Amount = customer.Amount;
             var options = new Stripe1.ChargeCreateOptions
             {
                 Amount = Convert.ToInt32(customer.Amount),
                 Currency = customer.CurrencyId == 1 ? "ILS" : "USD",
-                ReceiptEmail = "princesharma78@gmail.com",
+                ReceiptEmail = customer.Email,
                 Source = tokenresult.Id
                 // CustomerId = stripeCustomer.Id,
                 // Description = Convert.ToString(tParams.TransactionId), //Optional  
@@ -55,42 +59,8 @@ namespace PrinceLabz.Stripe.Controllers
 
             Stripe1.Charge charge = await _stripeGateway.CreateChargeAsync(options);
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("OrderDetail", "Home");
         }
-        //[HttpPost]
-        //public async Task<IActionResult> Index(Customer customer)
-        //{
-        //    Stripe1.CreditCardOptions card = new Stripe1.CreditCardOptions();
-        //    card.Name = customer.FirstName + " " + customer.LastName;
-        //    card.Number = customer.CardNumber;
-        //    card.ExpYear = customer.ExpYear;
-        //    card.ExpMonth = customer.ExpMonth;
-        //    card.Cvc = customer.Cvc;
-        //    //Assign Card to Token Object and create Token  
-        //    Stripe1.TokenCreateOptions token = new Stripe1.TokenCreateOptions();
-        //    token.Card = card;
-
-        //    Stripe1.Token tokenresult=await _stripeGateway.GetTokenAsync(card);
-
-        //    Stripe1.CustomerCreateOptions myCustomer = new Stripe1.CustomerCreateOptions();
-        //    myCustomer.Email = customer.Email;
-        //   // myCustomer.SourceToken = tokenresult.Id;
-
-        //    Stripe1.Customer stripeCustomer =await _stripeGateway.CreateCustomerAsync(myCustomer);
-
-        //    var options = new Stripe1.ChargeCreateOptions
-        //    {
-        //        Amount = Convert.ToInt32(customer.Amount),
-        //        Currency = customer.CurrencyId == 1 ? "ILS" : "USD",
-        //        ReceiptEmail = customer.Email,
-        //       // CustomerId = stripeCustomer.Id,
-        //       // Description = Convert.ToString(tParams.TransactionId), //Optional  
-        //    };
-
-        //    Stripe1.Charge charge = await _stripeGateway.CreateChargeAsync(options);
-
-        //    return RedirectToAction("Index", "Home");
-        //}
 
         public IActionResult Privacy()
         {
